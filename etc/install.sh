b1d833afb4ec81cfcce8990a58fe789114c50d3b
Jamie Mansfield <malgm@lapis.blue>
2015-06-13 18:43:23 +0100

    Fix install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -2,3 +2,4 @@
 
 # Install pip requirements
 pip install -r etc/requirements.txt
+pip install -e git+https://${GH_NAME}:${GH_TOKEN}@github.com/korobi/korobi_docs_theme.git#egg=korobi_docs_theme ?> /dev/null
